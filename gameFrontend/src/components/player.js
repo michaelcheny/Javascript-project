bc0ca13f6308fae715c71544d154f48eb1574207
Michael Chen <michaelchengaming@gmail.com>
2019-12-09 15:14:30 -0800

    correctly renders player icon and tweaked size to fit page better

--- gameFrontend/src/components/player.js
+++ gameFrontend/src/components/player.js
@@ -1,10 +1,10 @@
 class Player {
   constructor() {
-    this.x = 50;
-    this.y = height - 50;
+    this.x = 150;
+    this.y = height - 120;
   }
 
   show() {
-    image(playerImg, this.x, this.y, 50, 50);
+    image(playerImg, this.x, this.y, 120, 120);
   }
 }
