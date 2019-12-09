960a7d320f9d5227730ad51ff84527c57dede311
Michael Chen <michaelchengaming@gmail.com>
2019-12-08 22:40:45 -0800

    add a temporary square placeholder for the model.

--- gameFrontend/src/components/sketch.js
+++ gameFrontend/src/components/sketch.js
@@ -8,14 +8,16 @@
 //     rect(this.x, this.y, 50, 50);
 //   }
 // }
-
+let player;
 function setup() {
-  let myCanvas = createCanvas(1100, 1000);
+  let myCanvas = createCanvas(1000, 900);
   myCanvas.parent("game-container");
+  player = new Player();
 }
 
 function draw() {
   background(220);
+  player.show();
 }
 
 // let model;
