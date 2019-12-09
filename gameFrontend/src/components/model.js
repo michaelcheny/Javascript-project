34ffdabeb5c2b44e6ca1dfb1c7f9e8ac8ef221b7
Michael Chen <michaelchengaming@gmail.com>
2019-12-08 20:04:04 -0800

    add p5 js library to start making shapes and stuff for game. add 2 files for sketch and model for game

---
+++ gameFrontend/src/components/model.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor() {
+    this.x = 50;
+    this.y = height - 50;
+  }
+
+  show() {
+    rect(this.x, this.y, 50, 50);
+  }
+}
---
+++ gameFrontend/src/components/sketch.js
@@ -0,0 +1,29 @@
+// class Player {
+//   constructor() {
+//     this.x = 50;
+//     this.y = height - 50;
+//   }
+
+//   show() {
+//     rect(this.x, this.y, 50, 50);
+//   }
+// }
+
+function setup() {
+  createCanvas(1000, 1000);
+}
+
+function draw() {
+  background(220);
+}
+
+// let model;
+// function setup() {
+//   createCanvas(800, 900);
+//   model = new model();
+// }
+
+// function draw() {
+//   background(300);
+//   model.show();
+// }
