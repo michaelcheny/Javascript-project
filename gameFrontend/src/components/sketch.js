3117c54ccc976674f887517f00e5e35ccb46ff1e
Michael Chen <michaelchengaming@gmail.com>
2019-12-09 21:10:01 -0800

    try to set constrains but failed, try again

--- gameFrontend/src/components/sketch.js
+++ gameFrontend/src/components/sketch.js
@@ -1,6 +1,17 @@
 let player;
 let backgroundImg;
 let playerImg;
+let inner = edge + radius;
+
+let leftWall = 0;
+let rightWall = 900;
+
+// let xc = constrain(player, leftWall, rightWall);
+
+// a = 65
+// d = 68
+// left arrow = 37
+// right arrow = 39
 
 function preload() {
   backgroundImg = loadImage("src/images/court-background.jpg");
@@ -11,17 +22,17 @@ function setup() {
   let myCanvas = createCanvas(1000, 900);
   myCanvas.parent("game-container");
   player = new Player();
+  // let constrainedPlayer = constrain(player, leftWall, rightWall);
 }
 
 function keyReleased() {
-  // if (keyCode === 65 || keyCode === 37 || keyCode === 68 || keyCode === 39) {
-  player.setDirection(0);
-  // }
+  if (key !== " ") {
+    player.setDirection(0);
+  }
 }
 
 function keyPressed() {
   if (keyCode === 65 || keyCode === 37) {
-    // player.goLeft();
     player.setDirection(-1);
   } else if (keyCode === 68 || keyCode === 39) {
     player.setDirection(+1);
@@ -30,17 +41,7 @@ function keyPressed() {
 
 function draw() {
   background(backgroundImg);
+
   player.show();
   player.move();
 }
-
-// let model;
-// function setup() {
-//   createCanvas(800, 900);
-//   model = new model();
-// }
-
-// function draw() {
-//   background(300);
-//   model.show();
-// }
