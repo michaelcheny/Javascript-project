9ad14db980bb201396809f60dd539b58f1672117
Michael Chen <michaelchengaming@gmail.com>
2019-12-11 15:19:13 -0800

    redo backend with games and players

--- backend-api/app/controllers/api/v1/games_controller.rb
+++ backend-api/app/controllers/api/v1/games_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::GamesController < ApplicationController
 
-  
+  def index
+    @games = Game.all
+
+    render json: @games, status: 200
+  end
+
+  private
+
+  def game_params
+    params.require(:games).permit(:score, :rating, :player_id)
+  end
   
 end
