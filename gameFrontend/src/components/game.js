97f68de3b12fb0508f92cd00e2a5cca30922c13b
Michael Chen <michaelchengaming@gmail.com>
2019-12-13 21:25:52 -0800

    refactor the game class by putting the arrays of opponents into an array

--- gameFrontend/src/components/game.js
+++ gameFrontend/src/components/game.js
@@ -9,56 +9,68 @@ class Game {
 
   start() {
     this.head = new Harden(this);
-
-    // this.head.draw(this.ctx);
-
     this.interval = setInterval(() => {
       const rand = Math.floor(Math.random() * 5);
       if (rand < 3) {
         this.defence = new Defence(this);
         this.defenders.push(this.defence);
-        // this.defence.draw(this.ctx);
-        // this.collision = new Collision(this.head, this.defence);
       }
       if (rand < 1) {
         this.avoidCharge = new Charge(this);
         this.allCharge.push(this.avoidCharge);
-        // this.avoidCharge.draw(this.ctx);
       }
     }, 500);
+
+    this.gameObjects = [this.defenders, this.allCharge];
+    // console.log(this.gameObjects);
   }
 
   update(changeInTime) {
     this.head.update(changeInTime);
 
-    for (let d of this.defenders) {
-      d.update(changeInTime);
-      const outOfBound = this.defenders.filter(d => {
-        if (d.location !== undefined) d.location.y > 900;
-      });
-    }
-    for (let charge of this.allCharge) {
-      charge.update(changeInTime);
-      const out = this.allCharge.filter(c => {
-        if (c.location !== undefined) c.location.y > 900;
-      });
-    }
+    this.gameObjects.forEach(opponents => {
+      for (let opponent of opponents) {
+        opponent.update(changeInTime);
+        const outOfBound = opponents.filter(o => {
+          if (o.location !== undefined) o.location.y > 900;
+        });
+      }
+    });
+
+    //   for (let d of this.defenders) {
+    //     d.update(changeInTime);
+    //     const outOfBound = this.defenders.filter(d => {
+    //       if (d.location !== undefined) d.location.y > 900;
+    //     });
+    //   }
+    //   for (let charge of this.allCharge) {
+    //     charge.update(changeInTime);
+    //     const out = this.allCharge.filter(c => {
+    //       if (c.location !== undefined) c.location.y > 900;
+    //     });
+    //   }
   }
 
   draw(ctx) {
     this.head.draw(ctx);
 
-    for (let d of this.defenders) {
-      d.draw(ctx);
-      // const outOfBound = this.defenders.filter(d => {
-      //   if (d.location !== undefined) d.location.y > 900;
-      // });
-    }
-    for (let charge of this.allCharge) {
-      charge.draw(ctx);
-      // const out = this.allCharge.filter(c => {
-      //   if (c.location !== undefined) c.location.y > 900;
-      // });
-    }
+    this.gameObjects.forEach(opponents => {
+      for (let d of this.defenders) {
+        d.draw(ctx);
+      }
+    });
+
+    // for (let d of this.defenders) {
+    //   d.draw(ctx);
+    //   // const outOfBound = this.defenders.filter(d => {
+    //   //   if (d.location !== undefined) d.location.y > 900;
+    //   // });
+    // }
+    // for (let charge of this.allCharge) {
+    //   charge.draw(ctx);
+    //   // const out = this.allCharge.filter(c => {
+    //   //   if (c.location !== undefined) c.location.y > 900;
+    //   // });
+    // }
   }
 }
