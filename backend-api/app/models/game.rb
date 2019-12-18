3631665b9f6754c53ec2a66eb2d2d289bcea55a0
Michael Chen <michaelchengaming@gmail.com>
2019-12-18 11:29:14 -0800

    add gamesadapter class to fetch games json

--- backend-api/app/models/game.rb
+++ backend-api/app/models/game.rb
@@ -6,5 +6,9 @@ class Game < ApplicationRecord
     order(score: :desc).limit(5)
   end
   
+  def self.top_5_scores_today
+    where(updated_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).order(score: :desc).limit(5)
+  end
+
   
 end
---
+++ gameFrontend/src/adapters/GamesAdapter.js
@@ -0,0 +1,16 @@
+class GamesAdapter {
+  constructor() {
+    this.baseUrl = "http://localhost:3000//api/v1/games";
+  }
+
+  async getGames() {
+    try {
+      const res = await fetch(this.baseUrl);
+      const data = await res.Json();
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+      return "Check if your is running.";
+    }
+  }
+}
