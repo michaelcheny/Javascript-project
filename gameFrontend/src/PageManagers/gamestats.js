ef4c43b36b27191578fde2e0ccff33f2a9aca52b
michael chen <michaelchengaming@gmail.com>
2019-12-31 14:14:13 -0800

    delete unused commented out code

--- gameFrontend/src/PageManagers/gamestats.js
+++ gameFrontend/src/PageManagers/gamestats.js
@@ -10,9 +10,6 @@ class GameStats {
     this.top5AllTime_div = document.getElementById("top-5-all-time");
     this.top5Today_div = document.getElementById("top-5-today");
     this.averageRating_div = document.getElementById("average-rating");
-
-    this.inputForm_div = document.getElementById("new-name-form");
-    this.nameInput = document.getElementById("player-name");
   }
 
   async fetchAndLoadGameStats() {
@@ -27,22 +24,12 @@ class GameStats {
       for (let game of games.top_5_today) {
         this.renderTop5Today(game);
       }
-      // console.log(games.average_rating);
       this.renderAverageRating(games.average_rating);
     } catch (error) {
       console.log(error.message);
       return "Check to see if your server is up and running.";
     }
   }
-
-  // saveGame() {
-  //   this.inputForm_div.addEventListener("submit", e => {
-  //     e.preventDefault();
-  //     // console.log(e);
-  //     this.adapter.saveGame(game);
-  //     // ("addfunctiontoallowposttobackend");
-  //   });
-  // }
 
   renderRecentScores(game) {
     this.recentScores_div.innerHTML += `
