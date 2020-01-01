b5fe270375e37106f93cfe00c46b8c4a0bea8102
michael chen <michaelchengaming@gmail.com>
2019-12-31 16:15:32 -0800

    change active_record default timezone to local

--- backend-api/config/application.rb
+++ backend-api/config/application.rb
@@ -24,6 +24,10 @@ module BackendApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Set time zone to Pacific Time.
+    config.time_zone = 'Pacific Time (US & Canada)'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
