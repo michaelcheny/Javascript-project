c2298d3f1982e098de74cdaea52bb5e5433635a0
michael chen <michaelchengaming@gmail.com>
2020-01-03 17:25:41 -0800

    change menu page with some added instructions for playing the game

--- gameFrontend/src/components/screenmessages.js
+++ gameFrontend/src/components/screenmessages.js
@@ -15,10 +15,23 @@ class ScreenMessages {
     ctx.rect(0, 0, game.gameWidth, game.gameHeight);
     ctx.fillStyle = "rgba(0,0,0,1)";
     ctx.fill();
-    ctx.font = "60px Arial";
+    ctx.font = "30px Arial";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
-    ctx.fillText(`Hi, ${game.player}`, game.gameWidth / 2, game.gameHeight / 2);
+    ctx.fillText(`Hi, ${game.player}`, game.gameWidth / 2, 0 + 150);
+
+    const defender = document.getElementById("draymond-defence");
+    ctx.drawImage(defender, game.gameWidth / 2 - 75, game.gameHeight / 2, 150, 150);
+    ctx.fillText("+100 points", game.gameWidth / 2, game.gameHeight / 2 - 20);
+
+    const charge = document.getElementById("steven-adams-charge");
+    ctx.drawImage(charge, game.gameWidth * 0.25 - 75, game.gameHeight / 2, 150, 150);
+    ctx.fillText("-1 foul", game.gameWidth * 0.25, game.gameHeight / 2 - 20);
+
+    const ref = document.getElementById("ref");
+    ctx.drawImage(ref, game.gameWidth / 1.3 - 75, game.gameHeight / 2, 150, 150);
+    ctx.fillText("+1 foul", game.gameWidth / 1.3, game.gameHeight / 2 - 50);
+    ctx.fillText("+300 points", game.gameWidth / 1.3, game.gameHeight / 2 - 20);
   }
 
   showPausedMenu(ctx, game) {
