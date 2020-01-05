d1f7dedfe2175a6d6f4d24eb0ee3dbb925f5667c
michael chen <michaelchengaming@gmail.com>
2020-01-05 05:00:31 -0800

    change game screen background color to less dark color

--- gameFrontend/src/components/screenmessages.js
+++ gameFrontend/src/components/screenmessages.js
@@ -3,9 +3,9 @@ class ScreenMessages {
 
   showIntro(ctx, game) {
     ctx.rect(0, 0, game.gameWidth, game.gameHeight);
-    ctx.fillStyle = "rgba(0,0,0,1)";
+    ctx.fillStyle = "rgba(25,25,25,1)";
     ctx.fill();
-    ctx.font = "60px Yoster Island";
+    ctx.font = "60px Arcade";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
     ctx.fillText("Enter Your Name", game.gameWidth / 2, game.gameHeight / 2);
@@ -13,9 +13,9 @@ class ScreenMessages {
 
   showMainMenu(ctx, game) {
     ctx.rect(0, 0, game.gameWidth, game.gameHeight);
-    ctx.fillStyle = "rgba(0,0,0,1)";
+    ctx.fillStyle = "rgba(25,25,25,1)";
     ctx.fill();
-    ctx.font = "30px Yoster Island";
+    ctx.font = "30px Arcade";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
     ctx.fillText(`Hi, ${game.player}`, game.gameWidth / 2, 0 + 150);
@@ -33,7 +33,7 @@ class ScreenMessages {
     ctx.fillText("+1 foul", game.gameWidth / 1.3, game.gameHeight / 2 - 50);
     ctx.fillText("+500 points", game.gameWidth / 1.3, game.gameHeight / 2 - 20);
 
-    ctx.font = "20px Yoster Island";
+    ctx.font = "20px Arcade";
     ctx.fillText(
       "This is not a knock on Harden, press `enter` or `click` on screen to play",
       game.gameWidth / 2,
@@ -43,9 +43,9 @@ class ScreenMessages {
 
   showPausedMenu(ctx, game) {
     ctx.rect(0, 0, game.gameWidth, game.gameHeight);
-    ctx.fillStyle = "rgba(0,0,0,0.5)";
+    ctx.fillStyle = "rgba(25,25,25,0.5)";
     ctx.fill();
-    ctx.font = "100px Yoster Island";
+    ctx.font = "100px Arcade";
     ctx.fillStyle = "yellow";
     ctx.textAlign = "center";
     ctx.fillText("Time Out", game.gameWidth / 2, game.gameHeight / 2);
@@ -53,16 +53,16 @@ class ScreenMessages {
 
   showGameOver(ctx, game) {
     // ctx.rect(0, 0, game.gameWidth, game.gameHeight);
-    ctx.fillStyle = "rgba(0,0,0,1)";
+    ctx.fillStyle = "rgba(25,25,25,1)";
     ctx.fill();
-    ctx.font = "100px Yoster Island";
+    ctx.font = "100px Arcade";
     ctx.fillStyle = "red";
     ctx.textAlign = "center";
     ctx.fillText("Game Over", game.gameWidth / 2, game.gameHeight / 2);
   }
 
   showScoreAndFouls(ctx, game, score, fouls) {
-    ctx.font = "20px Yoster Island";
+    ctx.font = "20px Arcade";
     ctx.textAlign = "center";
     ctx.fillText("Score: " + score, 100, 40);
     ctx.fillText("Fouls Remaining: " + fouls, game.gameWidth - 140, 40);
