2904322ed2bb93cec267f7b0265477ba3aece278
michael chen <michaelchengaming@gmail.com>
2020-01-08 20:58:02 -0800

    add a falling object class that allows defender to inherit from

--- gameFrontend/src/components/defence.js
+++ gameFrontend/src/components/defence.js
@@ -1,9 +1,39 @@
-class Defence {
+// class Defence {
+//   constructor(game) {
+//     this.size = {
+//       x: this.height,
+//       y: this.width
+//     };
+//     const randomSpawnPosition = Math.floor(
+//       Math.random() * (game.gameWidth - this.size.x)
+//     );
+//     // sets this.speed to a random number between 500 and 700.
+//     this.speed = Math.random() * (700 - 500) + 500;
+//     this.position = {
+//       x: randomSpawnPosition,
+//       y: -200
+//     };
+//     this.collided = false;
+//   }
+
+//   update(changeInTime) {
+//     if (!changeInTime) return;
+//     this.position.y += (this.speed * changeInTime) / 1000;
+//   }
+
+//   draw(ctx) {
+//     this.img = document.getElementById("draymond-defence");
+//     ctx.drawImage(this.img, this.position.x, this.position.y, this.size.x, this.size.y);
+//   }
+// }
+
+class Defence extends FallingObject {
   constructor(game) {
-    this.game = game;
+    super(game);
+    this.img = document.getElementById("draymond-defence");
     this.size = {
-      x: 120,
-      y: 120
+      x: 140,
+      y: 140
     };
     const randomSpawnPosition = Math.floor(
       Math.random() * (game.gameWidth - this.size.x)
@@ -14,16 +44,5 @@ class Defence {
       x: randomSpawnPosition,
       y: -200
     };
-    this.collided = false;
-  }
-
-  update(changeInTime) {
-    if (!changeInTime) return;
-    this.position.y += (this.speed * changeInTime) / 1000;
-  }
-
-  draw(ctx) {
-    this.img = document.getElementById("draymond-defence");
-    ctx.drawImage(this.img, this.position.x, this.position.y, this.size.x, this.size.y);
   }
 }
---
+++ gameFrontend/src/components/fallingobject.js
@@ -0,0 +1,26 @@
+class FallingObject {
+  constructor(game) {
+    // this.size = {
+    //   x: 120,
+    //   y: 120
+    // };
+    // const randomSpawnPosition = Math.floor(Math.random() * (game.gameWidth - this.size.x));
+    // sets this.speed to a random number between 500 and 700.
+    // this.speed = Math.random() * (700 - 500) + 500;
+    // this.position = {
+    //   x: randomSpawnPosition,
+    //   y: -200
+    // };
+    this.collided = false;
+  }
+
+  update(changeInTime) {
+    if (!changeInTime) return;
+    this.position.y += (this.speed * changeInTime) / 1000;
+  }
+
+  draw(ctx) {
+    this.img = document.getElementById("draymond-defence");
+    ctx.drawImage(this.img, this.position.x, this.position.y, this.size.x, this.size.y);
+  }
+}
